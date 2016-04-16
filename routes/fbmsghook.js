9b3e084f8924f3d0f7f37b0a07b025fc81ef28dd
DB Peng <db.peng@ubitus.net>
2016-04-15 17:07:45 -0700

    follow the fb tutorial

--- routes/fbmsghook.js
+++ routes/fbmsghook.js
@@ -10,4 +10,20 @@ router.get('/', function(req, res) {
   	res.send('Error, wrong validation token');
 });
 
+router.post('/', function(req, res){
+	messaging_events = req.body.entry[0].messaging;
+
+	for (i = 0; i < messaging_events.length; i++) {
+	    event = req.body.entry[0].messaging[i];
+	    sender = event.sender.id;
+	    if (event.message && event.message.text) {
+	      text = event.message.text;
+	      // Handle a text message from this sender
+	      console.log(text);
+	    }
+  	}
+
+  	res.sendStatus(200);
+});
+
 module.exports = router;
